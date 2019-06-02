716179be6cefe63e4e980481709b19b94c641be7
Aniruddha2805 <aniruddha2805@gmail.com>
2019-06-02 12:08:45 +0530

    changes in code

--- src/test/java/seleniume2e/TestClass1.java
+++ src/test/java/seleniume2e/TestClass1.java
@@ -1,8 +1,13 @@
 package seleniume2e;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -11,14 +16,25 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import bsh.Remote;
+
 public class TestClass1 {
 
 	public static WebDriver driver;
 	
 	@BeforeMethod
 	public void launchDriver() {
+		DesiredCapabilities dr = null;
+		dr = DesiredCapabilities.chrome();
+		dr.setBrowserName("chrome");
+		dr.setPlatform(Platform.WIN8_1);
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-		driver = new ChromeDriver();
+		try {
+			driver = new RemoteWebDriver(new URL("http://192.168.0.106:4444/wd/hub"), dr);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
